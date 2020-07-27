0bbbe4e987b74a015230d43c4c453c3db34f2c06
DavidStephenss <dvdstephensd@gmail.com>
2020-07-27 16:25:23 -0500

    done

--- main.js
+++ main.js
@@ -10,13 +10,39 @@ const jobTypes = {
 };
 
 // Your code will go here
-class bob {
-  constructor(name, job, specialSkill, dob){
-    this.name = 'bob';
-    this.job = '6ft';
-    this.specialSkill = '250';
-    this.ship = '';
+//create a class for the crew
+class CrewMember {
+  constructor(name, job, specialSkill, ship){
+    this.name = name;
+    this.job = job;
+    this.specialSkill = specialSkill;
+    this.ship = ship;
   }
+  //make an entership function
+  enterShip(enter) {
+    this.ship = enter
+    enter.crew.push(this)
+  }
+  }
+
+  //make a class for ship
+ class Ship{
+    constructor(name, type, ability){
+    this.name = name;
+    this.type = type;
+    this.ability = ability;
+    this.crew = [];
+    // this.crew = crew;
+  }
+ missionStatement() {
+  if (this.crew.length == 0) {
+    return "Can't perform a mission yet."
+  } else {
+    return this.ability
+    }
+  }
+}
+
 
 
 
